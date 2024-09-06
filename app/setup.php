afffe5fac52e6a2d6be2e81f9a969c2f8c563842
Felipe <santospinheiro627@gmail.com>
2024-09-06 13:41:32 -0300

    Update alpine ans anothers imports

--- app/setup.php
+++ app/setup.php
@@ -1,6 +1,5 @@
 <?php
 
-
 add_action('wp_enqueue_scripts', 'fantastic_embedd_enqueue');
 
 function fantastic_embedd_enqueue(): void
@@ -13,20 +12,26 @@ function fantastic_embedd_enqueue(): void
     );
 
     wp_enqueue_script(
+        'alpine-js',
+        'https://cdn.jsdelivr.net/npm/alpinejs@3.14.1/dist/cdn.min.js',
+        [],
+        '1.0',
+        true,
+    );
+
+    wp_enqueue_script(
         'app-js',
         plugin_dir_url(__DIR__, 2) . '/../public/app.js',
         ['jquery'],
         '1.' . rand(),
         true,
     );
-}
 
+}
 
 add_shortcode('fembed', 'fantastic_embedd_shortcode');
 
-function fantastic_embedd_shortcode(): void 
+function fantastic_embedd_shortcode(): void
 {
-    // ...
+    require_once plugin_dir_path(__DIR__) . 'app/templates/index.php';
 }
-
-
