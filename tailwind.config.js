a9245e2eec4e08fe299c31e5efe86e248a699bb6
Felipe <santospinheiro627@gmail.com>
2024-09-11 15:58:02 -0300

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [ "./app/**/*.{html,js,php}" ],
+    content: [ "./app/**/*.{html,js,php}", "./docs/}" ],
     theme: {
         extend: {},
     },
